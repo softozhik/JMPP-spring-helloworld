3a530090733798a92926dc1b144a5d6cee2b2f28
softozhik <softozhik@live.ru>
2020-10-23 13:58:11 +0300

    вроде, всё...

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld hw1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld hw2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("helloworld: " + hw1.equals(hw2));
+        System.out.println("cat: " + cat1.equals(cat2));
+
+        //System.out.println(bean.getMessage());
     }
 }
